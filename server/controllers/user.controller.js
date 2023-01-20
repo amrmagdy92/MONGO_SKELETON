59cdee5f2ab20c5cc79dfea5cc90c3acf7985157
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-20 20:33:49 +0200

    Linted the file

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,26 @@
+import userModel from "../models/user.model"
+import errorHandler from "../helpers/dbErrorHandler"
+
 export default {
-    create: () => {},
-    list: () => {},
+    create: (req, res) => {
+        const user = new userModel(req.body)
+        user.save().then( () => {
+            return res.status(200).json({
+                message: "Successfully signed up!"
+            })
+        }).catch((err) => {
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            })
+        })
+    },
+    list: (req, res) => {
+        let usersList
+        userModel.find().select('firstName lastName email createdAt updatedAt').then( (users) => {
+            usersList = users
+            console.log(usersList)
+        })
+    },
     read: () => {},
     update: () => {},
     delete: () => {},
