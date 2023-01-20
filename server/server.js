d0f4182a96ebdbe18f872f2ba7af4127ee2dbc72
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-20 16:15:40 +0200

    imported and used dotenv in server base

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+import dotEnv from "dotenv"
 import express from "express"
 import { MongoClient } from "mongodb"
+
+dotEnv.config()
 
 const mongoURL = process.env.MONGODB_URI || `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@devcluster.b1dvlrj.mongodb.net/?retryWrites=true&w=majority`
 
