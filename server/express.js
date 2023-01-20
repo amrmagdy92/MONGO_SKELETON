3a2b01eb72dcc648b339d9b2d6eaf0682389b09e
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-20 17:27:42 +0200

    added production dependencies configuration to express file

--- server/express.js
+++ server/express.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv"
 import express from "express"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import compress from "compression"
+import cors from "cors"
+import helmet from "helmet"
 import { MongoClient } from "mongodb"
 import config from "../config/config"
 
@@ -13,6 +18,11 @@ MongoClient.connect(config.mongoURI).then(() => {
 
 const app = express()
 
-app.get('/', (req, res) => {res.send("hello world!!!")})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
 
 export default app
