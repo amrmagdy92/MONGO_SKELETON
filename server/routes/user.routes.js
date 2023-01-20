7cb69971e92b48c0ce8205b2f1c5e21bb6b8201f
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-20 20:35:34 +0200

    Modified the urls for the users router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,11 +3,11 @@ import userController from "../controllers/user.controller"
 
 const router = express.Router()
 
-router.route('/api/users')
+router.route('/')
     .get(userController.list)
     .post(userController.create)
 
-router.route('/api/users/:userId')
+router.route('/:userId')
     .get(userController.read)
     .put(userController.update)
     .delete(userController.delete)
