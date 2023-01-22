b259ce3178c16ec97492a427ea9bdeaa827da526
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-22 16:54:46 +0200

    Implemented express-jwt handler

--- server/express.js
+++ server/express.js
@@ -37,6 +37,15 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
 
+app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({"error": `${err.name}: ${err.message}`})
+    } else if (err) {
+        res.status(400).json({"error": `${err.name}: ${err.message}`})
+        console.log(err)
+    }
+})
+
 app.use('/', authRouter)
 app.use('/api/users', userRouter)
 
