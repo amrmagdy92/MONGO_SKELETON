9195f5333909df42109883a732a88f651533d71b
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-22 16:23:49 +0200

    updated autherntication router to reflect changes in express

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,7 @@ import authController from "../controllers/auth.controller"
 
 const router = express.Router()
 
-router.route('/signin').post(authController.signin)
-router.route('/signout').get(authController.signout)
+router.route('/auth/signin').post(authController.signin)
+router.route('/auth/signout').get(authController.signout)
 
 export default router
