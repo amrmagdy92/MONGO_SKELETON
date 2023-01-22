460909073010adead2e90b40d3c2fc8380f27b77
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-22 16:51:05 +0200

    implemented hasAuthorization

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -37,5 +37,14 @@ export default {
         secret: config.jwtSecret,
         userProperty: 'auth'
     }),
-    hasAuthorization: () => {}
+    hasAuthorization: (req, res, next) => {
+        const authorized = req.profile && req.auth && req.profile._id == req.auth._id
+        if (!authorized) {
+            return res.status('403').json({
+                error: "User is not authorized"
+            })
+        } else {
+            next()
+        }
+    }
 }
