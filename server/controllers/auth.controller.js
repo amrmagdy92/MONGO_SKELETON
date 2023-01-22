a64a0c9eb5c73c7f4be3183ab0e0e8d46af26016
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-22 16:14:14 +0200

    Implemented signin

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+import expressJWT from "express-jwt"
+import userModel from "../models/user.model"
+import config from "../../config/config"
+
+export default {
+    signin: async (req, res) => {
+        try {
+            let user = await userModel.findOne({"email": req.body.email})
+
+            if (!user) return res.status(401).json({error: "User not found"})
+            if (!user.authenticate(req.body.password)) return res.status(401).json({error: "Login credentials do not match"})
+
+            const token = jwt.sign({ _id: this._id }, config.jwtSecret)
+
+            res.cookie('t', token, { expire: new Date() + config.jwtExpiry})
+
+            return res.json({
+                token,
+                user: {
+                    _id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email
+                }
+            })
+        
+        } catch (err) {
+            return res.status('401').json({ error: "Could not sign in"})
+        }
+    },
+    signout: (req, res) => {},
+    requireSignin,
+    hasAuthorization: () => {}
+}
