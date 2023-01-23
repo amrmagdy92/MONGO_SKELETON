a347dde5f4b060f054ae7531bee8c596b7ac37a5
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-01-23 12:09:12 +0200

    Refactored the file to define the functions outside the export scope

--- server/helpers/user.helpers.js
+++ server/helpers/user.helpers.js
@@ -1,26 +1,34 @@
 import userModel from "../models/user.model"
+import crypto from "crypto"
+
+function encryptPassword (password, salt) {
+    if (password == '' || password == null || password == undefined) return ''
+    try {
+        return crypto
+            .createHmac('sha1', salt)
+            .update(password)
+            .digest('hex')
+    } catch (err) {
+        return err
+    }
+}
+
+async function authenticate (userID, password) {
+    let user = await userModel.findById(userID)
+    if (user) {
+        return encryptPassword(password, user.user_salt) === user.hashed_password
+    } else {
+        return "User not found"
+    }
+
+}
+
+function makeSalt () {
+    return Math.round(new Date().valueOf() * Math.random()).toString()
+}
 
 export default {
-    authenticate: (userID, password) => {
-        userModel.findById(userID).then((user) => {
-            return encryptPassword(password, user.user_salt) === user.hashed_password
-        }).catch((err) => {
-            console.log(err)
-            return "User not found"
-        })
-    },
-    encryptPassword: (password, salt) => {
-        if (password == '' || password == null || password == undefined) return ''
-        try {
-            return crypto
-                .createHmac('sha1', salt)
-                .update(password)
-                .digest('hex')
-        } catch (err) {
-            return err
-        }
-    },
-    makeSalt: () => {
-        return Math.round(new Date().valueOf() * Math.random()).toString()
-    }
+    authenticate,
+    encryptPassword,
+    makeSalt
 }
